fdb1779e677c7e33e28502864c6422b32b5ab83f
Matthew Willems <matthewwillems2003@outlook.com>
2021-01-14 21:00:48 +0100

    webxr matthew

--- WebXr/Javascript.js
+++ WebXr/Javascript.js
@@ -4,9 +4,9 @@ controls.enableMovement();
 
 //models
 const Box = new Model('Box.obj');
-Box.setPosition(4.32912, 1.31293, -5.6555); 
-Box.setScale(0.1, 0.1, 0.1);
-Box.setRotation(0, -36.95, 0);
+Box.setPosition(0.984, 1.184, -7.352); 
+Box.setScale(0.045, 0.045, 0.045);
+Box.setRotation(0, -36.9, 0);
 
 //const Lichaam = new Model('Lichaam.obj');
 //Box.setPosition(4.32912, 1.31293, -5.6555); 
@@ -17,26 +17,31 @@ Box.setRotation(0, -36.95, 0);
 
 //voor,links
 const tafel = new Model('Tafel.obj');
-tafel.setPosition(-0.005, 0.073, -3.364); 
+tafel.setPosition(-0.045, 0.000, -6.701); 
 tafel.setScale(1.476, 1.670, 2.056 );
-tafel.setRotation(0.000, -87.739, 0.000);
+tafel.setRotation(0.000, -71.048, 0.000);
 
 //voor, rechts 
 const tafel2 = new Model('Tafel.obj');
-tafel2.setPosition(3.305, 0.073, -3.464); 
+tafel2.setPosition(3.305, 0.000, -0.781); 
 tafel2.setScale(1.476, 1.670, 2.056 );
-tafel2.setRotation(0.000, -87.739, 0.000);
-
-//Links, voor
-const tafel1 = new Model('Tafel.obj');
-tafel1.setPosition(-2.505, 0.073, -2.364); 
-tafel1.setScale(1.467, 1.670, 2.046);
-tafel1.setRotation(0, 0, 0);
+tafel2.setRotation(0.000, -96.404, 0.000);
 
 //Rechts, voor
 const tafel3 = new Model('Tafel.obj');
-tafel3.setPosition(5.305, 0.073, -3.464); 
+tafel3.setPosition(4.846, 0.000, -3.234); 
 tafel3.setScale(1.476, 1.670, 2.056 );
-tafel3.setRotation(0, 0, 0);
+tafel3.setRotation(0, -7.472, 0);
 
+//EZEL
+const palet = new Model('palet.obj');
+palet.setPosition(1.977, 1.140, -0.618); 
+palet.setScale(0.015, 0.015, 0.015 );
+palet.setRotation(0, 0, 0);
+
+//ROMP
+const lichaam = new Model('lichaam.obj');
+lichaam.setPosition(-1.708, 0, -6.197); 
+lichaam.setScale(0.04, 0.04, 0.04 );
+lichaam.setRotation(0, -16.849, 0);
 
